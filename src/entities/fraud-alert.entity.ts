f0be9b5f8a5c48f436d533382a43287fc2eb04cb
Muma Brian <brianmuma5@gmail.com>
2024-09-05 19:54:14 +0100

    .

--- src/entities/fraud-alert.entity.ts
+++ src/entities/fraud-alert.entity.ts
@@ -1,25 +1,42 @@
-import { Entity, Column, PrimaryGeneratedColumn, ManyToOne } from 'typeorm';
+import {
+	Entity,
+	Column,
+	PrimaryGeneratedColumn,
+	ManyToOne,
+	CreateDateColumn,
+	UpdateDateColumn,
+} from 'typeorm';
 import { Users } from './user.entity';
 import { Transaction } from './transaction.entity';
 import { FraudAlertStatus } from './enums/fraud-alert-status.enum';
 
 @Entity()
 export class FraudAlert {
-	@PrimaryGeneratedColumn()
-	id: number;
+	@PrimaryGeneratedColumn('uuid')
+	id: string;
 
 	@Column()
-	alertType: string;
+	alertType: string; // e.g., 'Large Transaction', 'Unusual Activity'
 
-	@Column({ type: 'enum', enum: FraudAlertStatus })
+	@Column({
+		type: 'enum',
+		enum: FraudAlertStatus,
+		default: FraudAlertStatus.PENDING,
+	})
 	status: FraudAlertStatus;
 
-	@Column()
+	@CreateDateColumn()
 	createdDate: Date;
+
+	@UpdateDateColumn()
+	updatedDate: Date;
 
 	@ManyToOne(() => Users, (user) => user.fraudAlerts)
 	user: Users;
 
 	@ManyToOne(() => Transaction, (transaction) => transaction.fraudAlerts)
 	transaction: Transaction;
+
+	@Column({ nullable: true })
+	additionalInfo: string; // Optional field to store more details about the alert
 }
